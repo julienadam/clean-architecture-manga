2a6ac0f93c87e5e343bc3255315f9ed27b354582
Julien Adam <julien.adam@gmail.com>
2023-04-14 21:25:25 +0200

    WIP, trying to get this to build
    So far, had to bump .NET version, and node version because the fsevents npm
    package is buggy on the old Node 9 version this project uses.

--- docker-compose/setup.ps1
+++ docker-compose/setup.ps1
@@ -1,5 +1,7 @@
 # 1. makecert
 
+mkdir https
+
 openssl req -x509 -newkey rsa:2048 -keyout ./https/localhost.key -out ./https/localhost.crt -days 365 -subj "/CN=wallet.local/O=wallet.local/C=US" -config ./ssl-selfsigned.cnf -passout pass:MyCertificatePassword
 openssl pkcs12 -export -out ./https/localhost.pfx -inkey ./https/localhost.key -in ./https/localhost.crt -name "Localhost selfsigned certificate" -password pass:MyCertificatePassword -passin pass:MyCertificatePassword
 openssl rsa -in ./https/localhost.key -out ./https/localhost.key -passin pass:MyCertificatePassword
